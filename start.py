2f6a7d6477590e256f24407f7017bdb414f70c32
Eruma <josleftover@hotmail.com>
2025-03-19 22:04:24 -0600

    interfaces

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from loginWin import LoginC  # Asegúrate de que 'login.py' es el archivo donde está la clase LoginC
+from loginWin import LoginC
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)  # Inicializar la aplicación PyQt5
-    ventana = LoginC()  # Instanciar el login
-    ventana.show()  # Mostrar la ventana de login
+    app = QApplication(sys.argv)
+    ventana = LoginC()
+    ventana.show()
     sys.exit(app.exec_())
